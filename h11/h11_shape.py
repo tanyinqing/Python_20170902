b0f84cbf611fccc273b2331d40d3bac3ce6f56fa
tanyinqing <289367447@qq.com>
2018-07-17 22:26:02 +0800

    “修改”

---
+++ h11/h11_shape.py
@@ -0,0 +1,23 @@
+
+class Shape(object):
+    def area(self):
+        print('in shape:area...')
+        pass
+    def perimeter(self):
+        print('in Shape:perimeter...')
+        pass
+class Circle(Shape):
+    def area(self):
+        print('in Circle:area...')
+# 继承父类
+class Square(Shape):
+    def print_info(self):
+        print('a new def in Square...')
+
+a_circle=Circle();
+a_square=Square();
+a_circle.area()
+a_circle.perimeter()
+a_square.area()
+a_square.perimeter()
+a_square.print_info()
